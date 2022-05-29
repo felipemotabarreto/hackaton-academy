c3dd2faa6a06ef450dcc82717fcdee558290d893
Franklyn Seabra <franklyn.seabra@osf.digital>
2022-05-29 17:53:46 -0300

    🐛 Fix some bugs

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ const Product = ({ name, description, image, price }) => {
   return (
     <div className="product-card">
       <p className="name">{name}</p>
-      <p className="desc">{description}</p>
+      <p className="description">{description}</p>
       <img className="image" src={image} alt="product" />
       <p className="price">{price}</p>
     </div>
